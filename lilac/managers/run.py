996d8761ea11a150769c64de0895d742e650ae99
habroptilus <x7deed53bc9k8@softbank.ne.jp>
2021-05-25 17:28:09 +0900

    optuna範囲変更

--- lilac/managers/run.py
+++ lilac/managers/run.py
@@ -1,5 +1,6 @@
 from lilac.experiment.arg_parse import get_experiment_parser
 from lilac.experiment.run_experiment import run_experiment
+from lilac.utils.utils import MyEncoder
 import json
 
 
@@ -12,7 +13,7 @@ def main():
     result, output_path = run_experiment(args)
 
     with open(output_path, "w") as f:
-        json.dump(result, f)
+        json.dump(result, f, cls=MyEncoder)
 
 
 if __name__ == "__main__":
